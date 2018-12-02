ace3775acceb55428d08e67224410c0131cfe4ac
Kyle Gagner <kyle@gagner.us>
2018-12-01 17:09:03 -0800

    completed first draft of code to satisfy section 4.1 requirements

---
+++ main.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sim_GLM import *
+
+def do_4_1():
+	print("4.1 Simulating a GLM")
+	# set up filters and stimulus
+	f = [20*math.exp(-t) for t in range(15)]
+	h = [-200*math.exp(-t) for t in range(15)]
+	b = -15
+	mu = 0.3
+	sigma = 0.1
+	s = np.concatenate([np.zeros(2000), sigma * np.random.randn(18000) + mu])
+	# simulate
+	y = sim_GLM(s, f, h, b)
+	# plot
+	plt.subplot(4, 1, 1)
+	plt.plot(f)
+	plt.title('stimulus filter')
+	plt.subplot(4, 1, 2)
+	plt.plot(h)
+	plt.title('self interaction filter')
+	plt.subplot(4, 1, 3)
+	plt.plot(s)
+	plt.title('stimulus')
+	plt.subplot(4, 1, 4)
+	plt.plot(y)
+	plt.title('response')
+	plt.tight_layout()
+	plt.show()
+
+do_4_1()
---
+++ sim_GLM.py
@@ -0,0 +1,29 @@
+import math
+import numpy as np
+import statsmodels.api as sm
+
+# takes stimulus s, stimulus filter f, self interaction filter h, offset b
+# returns simulated results of GLM
+def sim_GLM(s, f, h, b):
+	# the formula in section 3 suggests only past values in the stimulus are used
+	# this makes sense in the case of the self interaction filter, but not stimulus filter
+	# I suggest we ask in class whether the stimulus filter extends from t=0 or t=1
+	# assuming starts at t=1 for consistency with formula
+	
+	# filtered stimulus (padded according to formula discussed above)
+	x = np.convolve(s, np.concatenate([[0], f]))[0:len(s)]
+	# spike history: to make indexing easier, history is padded to negative time
+	# t in the range -len(h) to len(s)-1 inclusive
+	y = np.zeros(len(h) + len(s))
+	for i in range(len(h), len(h)+len(s)):
+		l = x[i - len(h)] + b
+		for j in range(len(h)):
+			l += y[i - j - 1] * h[j]
+		if l > 25:
+			l = 25
+		y[i] = np.random.poisson(math.exp(l))
+		# the instructions dictate we are to ensure no more than one spike per bin
+		if y[i] > 1:
+			y[i] = 1
+	# return the spike history sans zero padding
+	return y[len(h):]
