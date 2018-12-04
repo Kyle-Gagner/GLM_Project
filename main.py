20be4e9db9a6cfec66697ccbab797923317eaf7f
Kyle Gagner <kyle@gagner.us>
2018-12-04 15:49:39 -0800

    working on section 4.3

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 from sim_GLM import *
 from fit_GLM import *
 
-def do_4_1():
+def do_4():
+	# section 4.1
 	print("4.1 Simulating a GLM")
 	# set up filters and stimulus
-	f = [20*math.exp(-t) for t in range(15)]
-	h = [-200*math.exp(-t) for t in range(15)]
+	d = 15
+	f = [20*math.exp(-t) for t in range(d)]
+	h = [-200*math.exp(-t) for t in range(d)]
 	b = -15
 	mu = 0.3
 	sigma = 0.1
 	s = np.concatenate([np.zeros(2000), sigma * np.random.randn(18000) + mu])
 	# simulate
-	y = sim_GLM(s, f, h, b)
+	n = sim_GLM(s, f, h, b)
 	# plot
+	plt.figure()
 	plt.subplot(4, 1, 1)
 	plt.plot(f, label='True', linewidth=1)
 	plt.title('Stimulus Filter')
@@ -26,24 +29,88 @@ def do_4_1():
 	plt.plot(s, linewidth=0.25)
 	plt.title('Stimulus')
 	plt.subplot(4, 1, 4)
-	plt.plot(y, linewidth=0.25)
+	plt.plot(n, linewidth=0.25)
 	plt.title('Response')
 	plt.tight_layout()
-	plt.draw()
-	return (s, y)
-
-def do_4_2(s, n):
+	#plt.draw()
+	
+	# section 4.2
 	print("4.2 Fitting Parameters to a GLM")
-	d = 15  # filter length
-	f, h, b, se_f, se_h, se_b = fit_GLM(s, n, d)	
+	# run the fitter
+	fit_f, fit_h, fit_b, fit_se_f, fit_se_h, fit_se_b = fit_GLM(s, n, d)
+	# add to the existing plots
 	plt.subplot(4, 1, 1)
-	plt.errorbar(range(d), f, yerr=se_f, label='Estimate', linewidth=1)
+	plt.errorbar(range(d), fit_f, yerr=fit_se_f, label='Estimate', linewidth=1)
 	plt.legend()
+	plt.ylim((-1, 20))
 	plt.subplot(4, 1, 2)
-	plt.errorbar(range(d), h, yerr=se_h, label='Estimate', linewidth=1)
+	plt.errorbar(range(d), fit_h, yerr=fit_se_h, label='Estimate', linewidth=1)
 	plt.legend()
-	plt.ylim((-3, 3))
+	plt.ylim((-5, 1))
+	plt.show()
+	
+	# section 4.3
+	# these lists will contain the data points for each row of plots
+	data_by_length = []
+	data_by_spikes = []
+	data_by_offset = []
+	# they contain tuples, the following variables alias the indices in these tuples for convenience
+	x_value = 0
+	abs_b = 1
+	rmse_f = 2
+	rmse_h = 3
+	mse_f = 4
+	mse_h = 5
+	# compute data for the first two rows of plots
+	for l in np.repeat([int(x) for x in [1e3, 3e3, 1e4, 3e4]], 6):
+		s = sigma * np.random.randn(l) + mu
+		n = sim_GLM(s, f, h, b)
+		data = fitter_trial(s, n, f, h, b)
+		data_by_length.append((l, *data))
+		data_by_spikes.append((sum(n), *data))
+	# compute data for the last row of plots
+	for b in np.repeat([-14, -16, -18], 6):
+		s = sigma * np.random.randn(l) + mu
+		n = sim_GLM(s, f, h, b)
+		data_by_offset.append((sum(n), *fitter_trial(s, n, f, h, b)))
+	# describe the rows of plots (dataset, x label)
+	plotrows = [
+		(data_by_length, 'Number of Samples'),
+		(data_by_spikes, 'Spike Counts'),
+		(data_by_offset, 'Spike Counts')]
+	# describe the columns of plots ([(index 1, color 1, label 1), (index 2, color 2, label 2)], y label)
+	plotcols = [([(abs_b, 'blue', 'offset')], 'Offset Error'),
+		([(rmse_f, 'blue', 'stimulus filters'), (rmse_h, 'red', 'response filters')], 'RMSE'),
+		([(mse_f, 'blue', 'stimulus filters'), (mse_h, 'red', 'response filters')], 'MSE')]
+	plt.figure()
+	for row in range(3):
+		dataset, x_label = plotrows[row]
+		for col in range(3):
+			plt.subplot(3, 3, row * 3 + col + 1)
+			serieslist, y_label = plotcols[col]
+			for series in serieslist:
+				index, color, label = series
+				plt.scatter([x[0] for x in dataset], [x[index] for x in dataset], c=color)
+			plt.xscale('log')
+	plt.show()
 
-s, n = do_4_1()
-do_4_2(s, n)
-plt.show()
+def fitter_trial(s, n, f, h, b):
+	'''takes stimulus s, spiking history n, stimulus filter f, self interaction filter h, offset b
+	fits GLM to s and n, returning absolute error in b, RMSE of f and h, and MSE of f and h
+	only coefficients with <5 standard error are used in the error calculation for h'''
+	if len(f) != len(h):
+		raise ValueError("Expected len(f) == len(h)")
+	fit_f, fit_h, fit_b, fit_se_f, fit_se_h, fit_se_b = fit_GLM(s, n, len(f))
+	error_f = np.subtract(fit_f, f)
+	error_h = np.subtract(fit_h, h)
+	# fudge factor, do not accumulate very large errors
+	for i in range(len(h)):
+		if fit_se_h[i] >= 1000:
+			fit_se_h[i] = 0
+			error_h[i] = 0
+	return abs(fit_b - b), rms(error_f), rms(error_h), np.mean(fit_se_f), np.mean(fit_se_h)
+
+def rms(x):
+	return np.sqrt(np.mean(np.square(x)))
+
+do_4()
