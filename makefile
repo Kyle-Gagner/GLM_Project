a0a131d215e334f54bb2f40d6983d21404b3c6fb
Kyle Gagner <kyle@gagner.us>
2018-12-08 17:19:28 -0800

    minor nonfunctional change to sim_GLM.py, added section 5 summary of results, started analysis

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 report.pdf: report.tex figures4 figures5 figures6
 	pdflatex -interaction=nonstopmode -halt-on-error report.tex
 
-figures4: section4.py
+figures4: section4.py sim_GLM.py fit_GLM.py
 	python3 section4.py
 
-figures5: section5.py
+figures5: section5.py sim_GLM.py fit_GLM.py
 	python3 section5.py
 
 figures6: section6.py
--- sim_GLM.py
+++ sim_GLM.py
@@ -5,21 +5,16 @@ def sim_GLM(s, f, h, b):
 	''' takes stimulus s, stimulus filter f, self interaction filter h, offset b
 	returns simulated results of GLM
 	'''
-	# the formula in section 3 suggests only past values in the stimulus are used
-	# this makes sense in the case of the self interaction filter, but not stimulus filter
-	# I suggest we ask in class whether the stimulus filter extends from t=0 or t=1
-	# assuming starts at t=1 for consistency with formula
-	
-	# filtered stimulus (padded according to formula discussed above)
+	# filtered stimulus
 	x = np.convolve(s, np.concatenate([[0], f]))[0:len(s)]
 	# spike history: to make indexing easier, history is padded to negative time
 	# t in the range -len(h) to len(s)-1 inclusive
-	y = np.zeros(len(h) + len(s))
+	n = np.zeros(len(h) + len(s))
 	for i in range(len(h), len(h)+len(s)):
 		l = x[i - len(h)] + b
 		for j in range(len(h)):
-			l += y[i - j - 1] * h[j]
+			l += n[i - j - 1] * h[j]
 		# ensure no more than one spike per bin
-		y[i] = min(np.random.poisson(math.exp(l)), 1)
+		n[i] = min(np.random.poisson(math.exp(l)), 1)
 	# return the spike history sans zero padding
-	return y[len(h):]
+	return n[len(h):]
